1b9b44aff37909f62812f10dcd76f7ffd39a4b3d
Erik Olofsson <playde5555@gmail.com>
2024-10-12 23:26:57 +0200

    Added dbscan and more plots

--- models/prepare_data.py
+++ models/prepare_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def prepare_data():
+def prepare_data(short_day, mid_day, long_day, change_factor):
     column_order = ['Date', 'Bid', 'Ask', 'Opening price', 'High price', 'Low price', 'Closing price', 'Average price', 'Total volume', 'Turnover', 'Trades']
     data = pd.read_csv("../data/SHB_A-1999-01-06-2024-02-05.csv", sep=";", decimal=",", skiprows=1, usecols=column_order)
     
@@ -10,17 +10,17 @@ def prepare_data():
 
     data["Diff"] = data["Closing price"].diff()
 
-    FACTOR = 0.0135 # Profit to define target 0.014
+    # change_factor = 0.0135 # Profit to define target 0.014
 
-    SHORT_CHANGE_HORIZON = 5
-    MID_CHANGE_HORIZON = 20
-    LONG_CHANGE_HORIZON = 50
-    FACTOR = 0.014 # Profit to define target 0.0135
-    # FACTOR = 0
+    # short_day = 5
+    # mid_day = 20
+    # long_day = 50
+    # change_factor = 0.014 # Profit to define target 0.0135
+    # change_factor = 0
 
-    data["Short_day_change"] = (data["Closing price"] - data["Closing price"].shift(SHORT_CHANGE_HORIZON)) / SHORT_CHANGE_HORIZON
-    data["Mid_day_change"] = (data["Closing price"] - data["Closing price"].shift(MID_CHANGE_HORIZON)) / MID_CHANGE_HORIZON
-    data["Long_day_change"] = (data["Closing price"] - data["Closing price"].shift(LONG_CHANGE_HORIZON)) / LONG_CHANGE_HORIZON
+    data["Short_day_change"] = (data["Closing price"] - data["Closing price"].shift(short_day)) / short_day
+    data["Mid_day_change"] = (data["Closing price"] - data["Closing price"].shift(mid_day)) / mid_day
+    data["Long_day_change"] = (data["Closing price"] - data["Closing price"].shift(long_day)) / long_day
 
 
     data["t+1"] = data["Closing price"].shift(-1)
@@ -30,11 +30,11 @@ def prepare_data():
     data["t+3"] = data["Closing price"].shift(-3)
 
     conditions = [
-        (data["t+1"] > (data["Closing price"] * (1+FACTOR))) |
-        (data["t+2"] > (data["Closing price"] * (1+FACTOR))) |
-        (data["t+3"] > (data["Closing price"] * (1+FACTOR))) |
-        (data["t+4"] > (data["Closing price"] * (1+FACTOR))) |
-        (data["t+5"] > (data["Closing price"] * (1+FACTOR)))
+        (data["t+1"] > (data["Closing price"] * (1+change_factor))) |
+        (data["t+2"] > (data["Closing price"] * (1+change_factor))) |
+        (data["t+3"] > (data["Closing price"] * (1+change_factor))) |
+        (data["t+4"] > (data["Closing price"] * (1+change_factor))) |
+        (data["t+5"] > (data["Closing price"] * (1+change_factor)))
     ]
 
 
