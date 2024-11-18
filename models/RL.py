3bc36f72c9a9c948082374cd7862e9f7a2f728f2
Erik Olofsson <77723339+Balkongen@users.noreply.github.com>
2024-11-18 11:31:18 +0100

    Developed model

--- models/RL.py
+++ models/RL.py
@@ -5,7 +5,7 @@ from prepare_data import prepare_data
 
 class RL:
 
-    def __init__(self, data: pd.Dataframe, Q = None) -> None:
+    def __init__(self, data: pd.DataFrame, Q = None) -> None:
         self.data = data
         self.Q = Q
 
@@ -15,8 +15,9 @@ class RL:
         return None
     
     
-    def is_terminal_state(date: str) -> bool:
-        return None
+    def is_terminal_state(date, lastDate) -> bool:
+
+        return date == lastDate
 
 
     def get_next_action(self, current_state: int, epsilon: float) -> int:
@@ -29,8 +30,10 @@ class RL:
 
 def run(number_of_episodes, epsilone = 0, discount_factor = 0.2, learning_rate = 0.9):
     data = prepare_data(5, 20, 50, 0.014)
-
     data = data.iloc[4500:] # Remove some data
+    
+    last_date = data["Date"].tail(1)
+
     actions = ["buy", "hold", "sell"]
     env = RL(data=data)
 
@@ -39,10 +42,16 @@ def run(number_of_episodes, epsilone = 0, discount_factor = 0.2, learning_rate =
 
     for episode in range(number_of_episodes):
 
-
-        while not env.is_terminal_state(data["Date"]):
+        current_date = data["Date"].head(1)
+        while not env.is_terminal_state(current_date, last_date):
+            
+            
             return
 
 
     
     return None
+
+
+if __name__ == "__main__":
+    run(10)
