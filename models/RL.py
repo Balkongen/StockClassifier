83abc045ba9f50a62872299a9f2871c4de6d5053
Erik Olofsson <playde5555@gmail.com>
2024-11-19 19:15:15 +0100

    Further development

--- models/RL.py
+++ models/RL.py
@@ -10,14 +10,14 @@ class RL:
         self.Q = Q
 
 
-    def get_reward(date: str) -> int:
+    def get_reward(self, date, action) -> int:
         # return profit/loss as reward?
+        
         return None
     
     
-    def is_terminal_state(date, lastDate) -> bool:
-
-        return date == lastDate
+    def is_terminal_state(self, date, last_date) -> bool:
+        return date == last_date
 
 
     def get_next_action(self, current_state: int, epsilon: float) -> int:
@@ -32,24 +32,38 @@ def run(number_of_episodes, epsilone = 0, discount_factor = 0.2, learning_rate =
     data = prepare_data(5, 20, 50, 0.014)
     data = data.iloc[4500:] # Remove some data
     
-    last_date = data["Date"].tail(1)
+    last_date = data["Date"].tail(1).values
 
     actions = ["buy", "hold", "sell"]
     env = RL(data=data)
 
-    total_profit = 0
-    number_of_trades = 0
-
-    for episode in range(number_of_episodes):
-
-        current_date = data["Date"].head(1)
-        while not env.is_terminal_state(current_date, last_date):
-            
-            
-            return
-
-
+    profit_arr = []
     
+    for episode in range(number_of_episodes):
+        
+        total_profit = 0
+        number_of_trades = 0
+        day_count = 0
+       
+        current_day = data.iloc[day_count]
+        current_date = current_day["Date"]
+       
+        
+        while not env.is_terminal_state(current_date, last_date):
+            print(current_date)
+            
+            
+            action = env.get_next_action()
+            reward = env.get_reward(current_date, action)
+            
+            
+            # DO something before
+            day_count += 1
+            current_day = data.iloc[day_count]
+            current_date = current_day["Date"]
+            
+        profit_arr.append(total_profit)
+
     return None
 
 
